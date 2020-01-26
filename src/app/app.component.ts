671f81731faa259ba4b5666298728866f60fbc06
Travis Craft <tlcraft@users.noreply.github.com>
2020-01-26 11:47:48 -0700

    Update component to use a FormGroup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,15 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
-  title = 'weather-ng';
-
-  zipCode: number;
-
+export class AppComponent implements OnInit {
   weather: string;
+  form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      zipCode: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{5}(?:-[0-9]{4})?$')])
+    });
+  }
 
   getCurrentWeather(): void {
     const temp = 75;
     this.weather = `${this.zipCode} is currently ${temp} degrees`;
+  }
+
+  get zipCode() {
+    return this.form && this.form.get('zipCode').value;
   }
 }
