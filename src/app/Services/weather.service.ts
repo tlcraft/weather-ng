8b1b1d16fc9f0cba3044bd2a8aa66eeb54797081
Travis Craft <tlcraft@users.noreply.github.com>
2020-01-29 17:06:58 -0700

    Implement current weather call.

--- src/app/Services/weather.service.ts
+++ src/app/Services/weather.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getCurrentWeather(zipCode: string): number {
-      return 75;
+  getCurrentWeather(zipCode: string): Observable<string> {
+    const params = new HttpParams().set('zipCode', zipCode);
+    const currentWeather = this.http.get<string>(environment.apiUrl + '/currentWeather', { params: params });
+    return currentWeather;
   }
 
 }
