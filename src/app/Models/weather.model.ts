c0f7026905ed8f475876e98add6813fafbf1dc40
Travis Craft <tlcraft@users.noreply.github.com>
2020-02-08 15:51:24 -0700

    Add WeatherData interface.

--- src/app/Models/weather.model.ts
+++ src/app/Models/weather.model.ts
@@ -1,5 +1,12 @@
 export interface Weather {
-    weather: any[];
+    weather: WeatherData[];
     main: any;
     name: string;
+}
+
+export interface WeatherData {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
 }
