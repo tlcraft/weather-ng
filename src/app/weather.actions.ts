9c833b42c378ed5efd1214058fa5feb77040240d
Travis Craft <tlcraft@users.noreply.github.com>
2020-03-03 17:19:40 -0700

    Add WeatherAction class.

--- src/app/weather.actions.ts
+++ src/app/weather.actions.ts
@@ -1,7 +1,16 @@
-import { createAction, props } from '@ngrx/store';
+import { createAction, props, Action } from '@ngrx/store';
 
 export enum WeatherActionTypes {
     GetCurrentWeather = '[Weather] GetCurrentWeather'
+}
+
+export class WeatherAction implements Action {
+    readonly type = WeatherActionTypes.GetCurrentWeather;
+    zipCode: string;
+
+    constructor(zipCode: string) {
+        this.zipCode = zipCode;
+    }
 }
 
 export const getWeather = createAction(WeatherActionTypes.GetCurrentWeather, props<{ zipCode: string }>());
