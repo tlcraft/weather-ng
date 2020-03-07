c08b02ecf4eb6d35db63cc83888968f47b40ea27
Travis Craft <tlcraft@users.noreply.github.com>
2020-03-06 19:50:16 -0700

    Update weather effects.

--- src/app/store/effects/weather.effects.ts
+++ src/app/store/effects/weather.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
-import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Actions, ofType, Effect } from '@ngrx/effects';
 import { of } from 'rxjs';
-import { map, mergeMap, catchError } from 'rxjs/operators';
+import { map, catchError, exhaustMap } from 'rxjs/operators';
 import { WeatherService } from '../../Services/weather.service';
 import { getWeather, WeatherActionTypes } from '../actions/weather.actions';
 
@@ -9,14 +9,13 @@ import { getWeather, WeatherActionTypes } from '../actions/weather.actions';
 export class WeatherEffects {
     constructor(private actions$: Actions, private weatherService: WeatherService) {}
 
-    getCurrentWeather$ = createEffect(() => 
-        this.actions$.pipe(
+    @Effect()
+    getCurrentWeather$ = this.actions$.pipe(
             ofType(getWeather),
-            mergeMap(action => this.weatherService.getCurrentWeather(action.zipCode)
-                .pipe(
-                    map(weather => ({ type: WeatherActionTypes.GetCurrentWeather, payload: weather })),
-                    catchError(() => of({ type: WeatherActionTypes.GetCurrentWeather, payload: null }))
-                )
+            exhaustMap(action => this.weatherService.getCurrentWeather(action.zipCode)
+            .pipe(
+                map(weather => ({ type: WeatherActionTypes.GetCurrentWeather, payload: weather })),
+                catchError(() => of({ type: WeatherActionTypes.GetCurrentWeather, payload: null }))
             )
         )
     );
