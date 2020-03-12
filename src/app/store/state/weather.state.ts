67cbf2b70d36138853d34584233dc867bfc57af3
Travis Craft <tlcraft@users.noreply.github.com>
2020-03-12 17:30:09 -0600

    Update weather state.

--- src/app/store/state/weather.state.ts
+++ src/app/store/state/weather.state.ts
@@ -2,10 +2,12 @@ import { Weather } from 'src/app/Models/weather.model';
 
 export interface WeatherState {
     weather: Weather;
+    error: any;
 }
 
 export const initialWeatherState: WeatherState = {
-    weather: null
+    weather: null,
+    error: null
 };
 
 export function getInitialState(): WeatherState {
