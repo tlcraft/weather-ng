646f23ddb9cad413b0d5693ee1fd20e9b54c422f
Travis Craft <tlcraft@users.noreply.github.com>
2020-03-14 11:40:41 -0600

    Update reducer function.

--- src/app/store/reducers/weather.reducer.ts
+++ src/app/store/reducers/weather.reducer.ts
@@ -1,13 +1,29 @@
-import { createReducer, on } from '@ngrx/store';
-import { getWeather } from '../actions/weather.actions';
+import { WeatherActionTypes, WeatherActions } from '../actions/weather.actions';
 import { Weather } from '../../Models/weather.model';
+import { WeatherState } from '../state/weather.state';
 
 export const initialState: Weather = null;
 
-const _weatherReducer = createReducer(initialState, 
-    on(getWeather, state => state),
-);
-
-export function weatherReducer(state, action) {
-    return _weatherReducer(state, action);
+export function weatherReducer(state: WeatherState, action: WeatherActions) {
+    switch(action.type) {
+        case WeatherActionTypes.GET_CURRENT_WEATHER:
+            return { 
+                ...state, 
+                error: null 
+            };
+        case WeatherActionTypes.GET_CURRENT_WEATHER_SUCCESS:
+            return { 
+                ...state,
+                weather: action.payload.weather,
+                error: null
+            };
+        case WeatherActionTypes.GET_CURRENT_WEATHER_FAILURE:
+            return { 
+                ...state,
+                weather: null,
+                error: action.payload.error
+            };
+        default:
+            return state;
+    }
 }
