44076a51447a9b4dc7b50497388348179ffe6711
Travis Craft <tlcraft@users.noreply.github.com>
2020-03-15 12:53:20 -0600

    Organize module imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { EffectsModule } from '@ngrx/effects';
+import { Store, StoreModule } from '@ngrx/store';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { WeatherEffects } from './store/effects/weather.effects';
-import { Store } from '@ngrx/store';
-import { StoreModule } from '@ngrx/store';
 import { weatherReducer } from './store/reducers/weather.reducer';
 
 @NgModule({
