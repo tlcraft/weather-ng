74cd30fa5cdf4f644e0354bdafd48ef4ed75a4ce
Travis Craft <tlcraft@users.noreply.github.com>
2020-03-16 17:38:37 -0600

    Default initial weather state.

--- src/app/store/reducers/weather.reducer.ts
+++ src/app/store/reducers/weather.reducer.ts
@@ -1,14 +1,14 @@
 import { WeatherActionTypes, WeatherActions } from '../actions/weather.actions';
-import { WeatherState } from '../state/weather.state';
+import { WeatherState, getInitialState } from '../state/weather.state';
 
-export function weatherReducer(state: WeatherState, action: WeatherActions) {
+export function weatherReducer(state: WeatherState = getInitialState(), action: WeatherActions) {
     switch(action.type) {
         case WeatherActionTypes.GET_CURRENT_WEATHER:
-            return Object.assign({}, state, { error: null });
+            return Object.assign({} as WeatherState, state, { error: null });
         case WeatherActionTypes.GET_CURRENT_WEATHER_SUCCESS:
-            return Object.assign({}, state, { weather: action.payload.weather, error: null });
+            return Object.assign({} as WeatherState, state, { weather: action.payload.weather, error: null });
         case WeatherActionTypes.GET_CURRENT_WEATHER_FAILURE:
-            return Object.assign({}, state, { weather: null, error: action.payload.error });
+            return Object.assign({} as WeatherState, state, { weather: null, error: action.payload.error });
         default:
             return state;
     }
