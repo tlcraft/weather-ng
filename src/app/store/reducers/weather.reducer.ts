0aeb82f0e09e89eaa5ffd04fb4a18f98d21cf23c
Travis Craft <tlcraft@users.noreply.github.com>
2020-03-16 17:27:29 -0600

    Use object assign to create new state.

--- src/app/store/reducers/weather.reducer.ts
+++ src/app/store/reducers/weather.reducer.ts
@@ -1,28 +1,14 @@
 import { WeatherActionTypes, WeatherActions } from '../actions/weather.actions';
-import { Weather } from '../../Models/weather.model';
 import { WeatherState } from '../state/weather.state';
-
-export const initialState: Weather = null;
 
 export function weatherReducer(state: WeatherState, action: WeatherActions) {
     switch(action.type) {
         case WeatherActionTypes.GET_CURRENT_WEATHER:
-            return { 
-                ...state, 
-                error: null 
-            };
+            return Object.assign({}, state, { error: null });
         case WeatherActionTypes.GET_CURRENT_WEATHER_SUCCESS:
-            return { 
-                ...state,
-                weather: action.payload.weather,
-                error: null
-            };
+            return Object.assign({}, state, { weather: action.payload.weather, error: null });
         case WeatherActionTypes.GET_CURRENT_WEATHER_FAILURE:
-            return { 
-                ...state,
-                weather: null,
-                error: action.payload.error
-            };
+            return Object.assign({}, state, { weather: null, error: action.payload.error });
         default:
             return state;
     }
