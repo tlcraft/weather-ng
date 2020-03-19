3018d3dcf1b58cbce80b2c365afcac740c3bf41a
Travis Craft <tlcraft@users.noreply.github.com>
2020-03-18 21:26:23 -0600

    Add return type to weather reducer.

--- src/app/store/reducers/weather.reducer.ts
+++ src/app/store/reducers/weather.reducer.ts
@@ -1,14 +1,14 @@
 import { WeatherActionTypes, WeatherActions } from '../actions/weather.actions';
 import { WeatherState, getInitialState } from '../state/weather.state';
 
-export function weatherReducer(state: WeatherState = getInitialState(), action: WeatherActions) {
+export function weatherReducer(state: WeatherState = getInitialState(), action: WeatherActions): WeatherState {
     switch(action.type) {
         case WeatherActionTypes.GET_CURRENT_WEATHER:
-            return Object.assign({} as WeatherState, state, { error: null });
+            return Object.assign({}, state, { error: null });
         case WeatherActionTypes.GET_CURRENT_WEATHER_SUCCESS:
-            return Object.assign({} as WeatherState, state, { weather: action.payload.weather, error: null });
+            return Object.assign({}, state, { weather: action.payload.weather, error: null });
         case WeatherActionTypes.GET_CURRENT_WEATHER_FAILURE:
-            return Object.assign({} as WeatherState, state, { weather: null, error: action.payload.error });
+            return Object.assign({}, state, { weather: null, error: action.payload.error });
         default:
             return state;
     }
