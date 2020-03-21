fa2b1d3ceb599faea37a498193326516f5cd09b9
Travis Craft <tlcraft@users.noreply.github.com>
2020-03-20 19:21:13 -0600

    Revert currentWeather$ to Weather type.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { WeatherAction } from './store/actions/weather.actions';
 import { WeatherState } from './store/state/weather.state';
+import { Weather } from './Models/weather.model';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,7 @@ export class AppComponent implements OnInit {
   weather: string;
   iconSource: string;
   form: FormGroup;
-  currentWeather$: Observable<WeatherState> = this.store.select(state => state);
+  currentWeather$: Observable<Weather> = this.store.select(state => state.weather);
 
   constructor(private store: Store<WeatherState>) {}
 
@@ -30,10 +31,10 @@ export class AppComponent implements OnInit {
       this.store.dispatch( new WeatherAction( { zipCode: this.zipCode } ));
       this.currentWeather$.subscribe(
         currentWeather => {
-          if(currentWeather && currentWeather.weather && currentWeather.weather.weather) {
-            const icon = currentWeather.weather.weather[0] && currentWeather.weather.weather[0].icon;
+          if(currentWeather && currentWeather.weather) {
+            const icon = currentWeather.weather[0] && currentWeather.weather[0].icon;
             this.iconSource = icon ? `http://openweathermap.org/img/w/${icon}.png` : '';
-            this.weather = `It's ${currentWeather.weather.main && currentWeather.weather.main.temp} degrees in ${currentWeather.weather.name}!`;
+            this.weather = `It's ${currentWeather.main && currentWeather.main.temp} degrees in ${currentWeather.name}!`;
           }
         },
         error => {
