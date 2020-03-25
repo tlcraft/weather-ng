a5438b7feee2b3a48d105a9f8791cfd2cb496d1e
Travis Craft <tlcraft@users.noreply.github.com>
2020-03-24 19:02:41 -0600

    Update weather reducer for loading field.

--- src/app/store/reducers/weather.reducer.ts
+++ src/app/store/reducers/weather.reducer.ts
@@ -1,21 +1,21 @@
-import { WeatherActionTypes, WeatherActions } from '../actions/weather.actions';
+import { WeatherActionTypes, WeatherAction } from '../actions/weather.actions';
 import { WeatherState, getInitialState } from '../state/weather.state';
 
-export function weatherReducer(state: WeatherState = getInitialState(), action: WeatherActions): WeatherState {
+export function weatherReducer(state: WeatherState = getInitialState(), action: WeatherAction): WeatherState {
     switch(action.type) {
         case WeatherActionTypes.GET_CURRENT_WEATHER:
         {
-            const newState = Object.assign({}, state, { error: null });
+            const newState = Object.assign({}, state, { loading: true });
             return newState;
         }
         case WeatherActionTypes.GET_CURRENT_WEATHER_SUCCESS:
         {    
-            const newState = Object.assign({}, state, { currentWeather: action.payload.currentWeather, error: null });
+            const newState = Object.assign({}, state, { currentWeather: action.payload.currentWeather, loading: false });
             return newState;
         }
         case WeatherActionTypes.GET_CURRENT_WEATHER_FAILURE:
         {        
-            const newState = Object.assign({}, state, { currentWeather: null, error: action.payload.error });
+            const newState = Object.assign({}, state, { loading: false, error: action.payload.error });
             return newState;
         }
         default:
